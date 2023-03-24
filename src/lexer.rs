1f1f833ec0b71fac82fb7f17712bf8ce12ba312d
noki3138 <nikitaserov912@gmail.com>
2023-03-25 01:45:46 +0300

    lexer ready

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,4 @@
-use std::borrow::Cow;
+use std::{borrow::Cow, mem};
 
 use crate::parser::{OpType, Token, TokenValue};
 
@@ -17,18 +17,60 @@ enum IntermediateExpr {
 pub fn lex<I: IntoIterator<Item = Token>>(tokens: I) -> LexerResult {
     let mut tokens = tokens
         .into_iter()
-        .map(|x| IntermediateExpr::Token(x))
+        .map(|x| {
+            if let TokenValue::Num(num) = x.value {
+                IntermediateExpr::Owned(Expr::Number(num))
+            } else {
+                IntermediateExpr::Token(x)
+            }
+        })
         .collect::<Vec<_>>();
-    process(&mut tokens)
+    process(&mut tokens)?;
+    Ok(tokens
+        .into_iter()
+        .find_map(|x| {
+            if let IntermediateExpr::Owned(expr) = x {
+                Some(expr)
+            } else {
+                None
+            }
+        })
+        .unwrap())
 }
 
-fn process(tokens: &mut [IntermediateExpr]) -> LexerResult {
+fn process(tokens: &mut [IntermediateExpr]) -> Result<(), LexError> {
     while let Some((lpar_index, rpar_index)) = find_parenthesises(&tokens)? {
         tokens[lpar_index] = IntermediateExpr::Empty;
         tokens[rpar_index] = IntermediateExpr::Empty;
         process(&mut tokens[lpar_index + 1..rpar_index])?;
     }
-    todo!()
+    while let Some((op_index, arg_indexes)) = find_expr(&tokens)? {
+        if let IntermediateExpr::Token(
+            token @ Token {
+                value: TokenValue::Op(op),
+                ..
+            },
+        ) = tokens[op_index]
+        {
+            if arg_indexes.len() != 2 {
+                Err(LexError {
+                    token,
+                    value: LexErrorValue::InvalidNumberOfArguments,
+                })?
+            }
+            let lhs = mem::replace(&mut tokens[arg_indexes[0]], IntermediateExpr::Empty);
+            let rhs = mem::replace(&mut tokens[arg_indexes[1]], IntermediateExpr::Empty);
+            if let (IntermediateExpr::Owned(lhs), IntermediateExpr::Owned(rhs)) = (lhs, rhs) {
+                tokens[op_index] =
+                    IntermediateExpr::Owned(Expr::Binary(Box::new(lhs), Box::new(rhs), op));
+            } else {
+                unreachable!()
+            }
+        } else {
+            unreachable!()
+        }
+    }
+    Ok(())
 }
 
 fn find_parenthesises(tokens: &[IntermediateExpr]) -> Result<Option<(usize, usize)>, LexError> {
@@ -70,6 +112,37 @@ fn find_parenthesises(tokens: &[IntermediateExpr]) -> Result<Option<(usize, usiz
     }
 }
 
+fn find_expr(tokens: &[IntermediateExpr]) -> Result<Option<(usize, Vec<usize>)>, LexError> {
+    let op = tokens.iter().position(|x| {
+        if let IntermediateExpr::Token(Token {
+            value: TokenValue::Op(..),
+            ..
+        }) = x
+        {
+            true
+        } else {
+            false
+        }
+    });
+    if let Some(op) = op {
+        let args = tokens
+            .iter()
+            .skip(op + 1)
+            .enumerate()
+            .filter_map(|(index, x)| {
+                if let IntermediateExpr::Owned(..) = x {
+                    Some(index + op + 1)
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<_>>();
+        Ok(Some((op, args)))
+    } else {
+        Ok(None)
+    }
+}
+
 pub type LexerResult = Result<Expr, LexError>;
 
 #[derive(Clone, Copy, Debug)]
@@ -81,4 +154,5 @@ pub struct LexError {
 #[derive(Clone, Copy, Debug)]
 pub enum LexErrorValue {
     UnmatchedParenthesis,
+    InvalidNumberOfArguments,
 }
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ mod vm;
 fn main() {
     let expr = "+ (* 2 3) (/ 8 2))";
     let tokens = parser::parse(expr).unwrap();
-    for token in tokens {
+    for token in &tokens {
         println!("{token:?}")
     }
+    let ast = lexer::lex(tokens).unwrap();
+    println!("{ast:?}")
 }
